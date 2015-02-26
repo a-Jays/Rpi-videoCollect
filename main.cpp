d41f78c43b1f3b8ec4da9d9e69bf55ee98d083e0
Ajay Shankar <ajays@iiitd.ac.in>
2015-02-26 13:02:40 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <thread>
+#include <atomic>
+
 #include "frames.cpp"
 #include "gps_logger.cpp"
 
 using namespace std;
+
+int main()
+{
+  thread gps( gps_thread_entry );
+  frame_collect();					// continue in the same thread.
+  gps.join();						// wait for gps to stop.
+  cout<<"Threads joined. Exit."<<endl;
+  system("sudo shutdown -h now");
+  return 1;
+}
