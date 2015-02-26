9972b66233d8dd2a062b7d0bc03761d41d5de2e5
Ajay Shankar <ajays@iiitd.ac.in>
2015-02-26 13:10:49 +0530

    added loop for frame_collect

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
   thread gps( gps_thread_entry );
-  frame_collect();					// continue in the same thread.
+  while( frames_collect() != 1 );					// continue in the same thread. Loop for in case something's wrong there.
   gps.join();						// wait for gps to stop.
   cout<<"Threads joined. Exit."<<endl;
   system("sudo shutdown -h now");
